70e68ea609707d17546a0b82f9021bc5846fbce6
JustaGuy16-03 <112471387+JustaGuy16-03@users.noreply.github.com>
2023-04-17 18:12:59 +0545

    major update

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React, { useState } from "react";
-import { FiSearch, FiX } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 import Logo from "../public/logo.svg";
 import Link from "next/link";
 import Wrapper from "./Wrapper";
@@ -8,19 +8,11 @@ import useFetch from "@/hooks/useFetch";
 
 const Search = ({ setSearchModal }) => {
   const [query, setQuery] = useState("");
-  const [hasContent, setHasContent] = useState(false);
   const [showResults, setShowResults] = useState(false);
 
   const onChange = (e) => {
     setQuery(e.target.value);
-    setHasContent(e.target.value.length > 0);
     setShowResults(e.target.value.length > 0);
-  };
-
-  const resetSearch = () => {
-    setQuery("");
-    setHasContent(false);
-    setShowResults(false);
   };
 
   let { data } = useFetch(
@@ -45,9 +37,9 @@ const Search = ({ setSearchModal }) => {
         </div>
       </Wrapper>
       <div className="md:w-[900px] mx-auto mt-5 px-[10px]">
-        <div className="relative flex items-center mt-5">
+        <div className="flex items-center mt-5">
           <input
-            className="outline-0 border-b px-10 md:py-5 py-2 rounded-none md:text-2xl text-xl text-gray-900 placeholder:text-gray-400 w-full text-center"
+            className="outline-0 border-b px-10 md:py-5 py-2 rounded-none md:text-2xl text-xl text-gray-900 placeholder:text-gray-500 w-full text-center"
             type="text"
             placeholder="Search for Menus"
             value={query}
@@ -55,25 +47,13 @@ const Search = ({ setSearchModal }) => {
             autoComplete="off"
             autoFocus="on"
           />
-          {hasContent ? (
-            <FiX
-              className="absolute text-gray-400 left-3 cursor-pointer md:text-[28px] text-[24px]"
-              onClick={resetSearch}
-            />
-          ) : (
-            <FiSearch className="absolute text-gray-400 left-3 md:text-[26px] text-[22px]" />
-          )}
         </div>
         <div className="my-5">
           {showResults && (
             <div className=" bg-white w-full lg:min-h-fit min-h-[260px] px-1 py-1 z-[1] md:text-base text-sm font-medium">
               {data?.data?.length ? (
                 data?.data?.map((item) => (
-                  <div
-                    className="w-full flex"
-                    key={item.id}
-                    onClick={resetSearch}
-                  >
+                  <div className="w-full flex" key={item.id}>
                     <Link
                       href={`/product/${item.attributes.slug}`}
                       className="w-full px-5 py-3 hover:bg-black/[0.03] rounded border-b"
