ee6899397e56dea3ec84fad169ef6cb4b2fc47b5
JustaGuy16-03 <112471387+JustaGuy16-03@users.noreply.github.com>
2023-04-21 18:33:58 +0545

    login signup added

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = ({ authUser, signOut }) => {
       {authUser ? (
         <div className="cursor-pointer" onClick={handleClick}>
           <div className="relative">
-            <div className="w-8 md:w-[32px] h-8 md:h-[32px] object-cover rounded-full flex justify-center items-center overflow-hidden">
+            <div className="w-8 h-8 object-cover rounded-full flex justify-center items-center overflow-hidden">
               {authUser.profile ? (
                 <img src={authUser.profile} alt="" />
               ) : (
@@ -25,11 +25,11 @@ const Profile = ({ authUser, signOut }) => {
                 id="category-items"
                 className="bg-white absolute top-[34px] right-0 min-w-[150px] md:px-2 px-1 md:py-2 py-1 text-black shadow rounded-md border md:text-base text-sm"
               >
-                <li className="w-full md:h-10 h-8 flex justify-between items-center md:px-3 px-2 rounded-md whitespace-nowrap">
+                <li className="w-full h-10 flex justify-between items-center px-3 rounded-md whitespace-nowrap">
                   {authUser.username}
                 </li>
                 <li
-                  className="bg-black text-white md:h-10 h-8 flex items-center gap-2 md:px-3 px-2 transition-transform hover:bg-black/[0.8] active:scale-90 md:rounded-md rounded whitespace-nowrap cursor-pointer"
+                  className="bg-black text-white h-10 flex items-center gap-2 px-3 transition-transform hover:bg-black/[0.8] active:scale-90 md:rounded-md rounded whitespace-nowrap cursor-pointer"
                   onClick={signOut}
                 >
                   <GoSignOut /> Log out
